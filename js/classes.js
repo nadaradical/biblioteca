fe16a57928b194b295e810112df290176ce1cace
Bane Biddix <bane@wrongthink.net>
2020-03-12 19:39:03 +0000

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -12,16 +12,28 @@ Item.prototype.toString = function() {
 
 
 class Bookshelf{
-    constructor(item2tags, tag2items){
+    constructor(item2tags, tag2items, word2items){
         this.item2tags = item2tags;
         this.tag2items = tag2items;
+        this.word2items = word2items;
+        if (!localStorage.getItem('checked_elems')) {
+            localStorage.setItem('checked_elems', JSON.stringify([...new Set()]))
+        }
+        this.checked_elems = new Set(JSON.parse(localStorage.getItem('checked_elems')))
     }
 
 }
 
 Bookshelf.prototype.getItemsFromTag = function (tag) {
-    if (! (tag in this.tag2items)){
+    if (!(this.tag2items.has(tag))){
         return [];
     }
-    return this.tag2items[tag];
+    return this.tag2items.get(tag);
+};
+
+Bookshelf.prototype.getItemsFromWord = function (word) {
+    if (!(this.word2items.has(word))) {
+        return new Set();
+    }
+    return this.word2items.get(word);
 };
